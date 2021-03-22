76c6cfb92b971b4f82ebfbe2060b289d8ca481c3
Rafał Krawętek <rafal.krawetek@gmail.com>
2021-03-22 02:23:33 +0100

    Base of application

---
+++ app/src/main/java/com/example/noticemycar/LoginPanel.kt
@@ -0,0 +1,11 @@
+package com.example.noticemycar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginPanel : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_panel)
+    }
+}
---
+++ app/src/main/java/com/example/noticemycar/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.noticemycar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        login.setOnClickListener {
+            var logowanie: Intent = Intent(applicationContext, LoginPanel::class.java)
+            startActivity(logowanie)
+        }
+
+        register.setOnClickListener {
+            var rejestracja: Intent = Intent(applicationContext, RegisterPanel::class.java)
+            startActivity(rejestracja)
+        }
+    }
+}
